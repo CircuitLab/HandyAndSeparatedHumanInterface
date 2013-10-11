6908f21a740d10a1eb90220027f4f238fdfe6c01
Fukuma Shinichi <fukuma@caneretina.local>
2013-10-11 11:43:36 +0900

    mame

--- hashi_web/js/hashi_web.js
+++ hashi_web/js/hashi_web.js
@@ -1,1 +1,51 @@
-hashi_web.js
+var catchObj;
+var bMouseDown = false;
+
+function init(){
+	var height = window.innerHeight;
+	var width = window.innerWidth;
+	$("div#container")
+		.css("width",width)
+		.css("height",height);
+
+
+	$("<div>")
+		.addClass("div-sara")
+		.css("top",Math.random() * (height - 400))
+		.css("left",Math.random() * (width - 400))
+		.appendTo($("#container"));
+
+	for (var i = 0; i < 100; i++){
+		var mame = $("<div>")
+			.addClass("div-mame")
+			.css("top",Math.random() * (height - 20))
+			.css("left",Math.random() * (width - 20));
+		if (Math.random() > 0.5){
+			mame.addClass("mame1");
+		}
+		else{
+			mame.addClass("mame2");
+		}
+		mame.rotate(360 * Math.random() + "deg");
+		mame.appendTo($("#container"));
+	}
+
+	$("div.div-mame").mousedown(function(){
+		bMouseDown = true;
+		catchObj = $(this);
+	});
+
+	$("div#container").mousemove(function(e){
+		if (bMouseDown){
+			//console.log(catchObj);
+			catchObj
+			.css("top",e.clientY - 20)
+			.css("left",e.clientX - 20);
+		}
+	});
+
+
+	$("div#container").mouseup(function(){
+		bMouseDown = false;
+	});
+}
